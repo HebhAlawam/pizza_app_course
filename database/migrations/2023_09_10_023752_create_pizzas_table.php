14d09019e18a1040590fe5d6e900ecc948836bc5
Hebh <yaheaawam3@gmail.com>
2023-09-10 05:57:54 +0300

    DB_admin

--- database/migrations/2023_09_10_023752_create_pizzas_table.php
+++ database/migrations/2023_09_10_023752_create_pizzas_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('is_admin');
-            $table->rememberToken();
+            $table->string('image');
+            $table->text('descraption');
+            $table->decimal('small_price');
+            $table->decimal('medium_price');
+            $table->decimal('big_price');
+            $table->string('category_id');
             $table->timestamps();
         });
     }
@@ -28,8 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pizzas');
     }
 };
-
-            $table->integer('is_admin')
